1419a5e1cdd52757f5f94fd2c5fa3b32002e1e1f
GavinHome <yangxiaoming512@yeah.net>
2023-07-04 09:02:03 +0800

    [feat] improve test code

--- test/lib/component/index_test.dart
+++ test/lib/component/index_test.dart
@@ -3,11 +3,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'component_test.dart' as component;
 import 'lifecycle_test.dart' as lifecycle;
 import 'page_test.dart' as page;
+import 'route_test.dart' as route;
 
 void main() {
   group('redux_component_test', () {
     component.main();
     lifecycle.main();
     page.main();
+    route.main();
   });
 }
